19d7a8b9cbcb3dabfc0f2e819290b7d05e16a095
Group Twentythree <grouptwentythree2023>
2023-11-23 15:35:47 +0000

    handling login authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def Login(request):
-    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        if user:
+            print('Authenticated')
+        else:
+            print('Not authenticated')
+        
     return render(request, 'accounts/login.html')
+
+
+
+
+def logout_user(request):
+    logout(request)
+    
+    return render()
